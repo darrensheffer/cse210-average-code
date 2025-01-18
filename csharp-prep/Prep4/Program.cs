a31013741fcabb2682e4ed09e356aa87ba944d25
darrensheffer <she22028@byui.edu>
2025-01-17 18:21:43 -0700

    Finished Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            string userAnswer = Console.ReadLine();
+            userNumber = int.Parse(userAnswer);
+            
+            if (userNumber !=0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sumTotal = 0;
+        foreach (int number in numbers)
+        {
+            sumTotal += number;
+        }
+        Console.WriteLine($"The sum is: {sumTotal}.");
+
+        // double average = 0;
+        double average = ((double)sumTotal) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is {max}");
     }
 }
